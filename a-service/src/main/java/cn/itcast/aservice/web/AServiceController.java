0f68f0d24d3ddd7268820626bd82c1b6396d364e
yinchuang <1933740841@qq.com>
2021-05-29 09:55:20 +0800

    add import Autowired

--- a-service/src/main/java/cn/itcast/aservice/web/AServiceController.java
+++ a-service/src/main/java/cn/itcast/aservice/web/AServiceController.java
@@ -1,6 +1,7 @@
 package cn.itcast.aservice.web;
 
 import cn.itcast.aservice.feign.BServiceFeign;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
--- b-service/src/main/java/cn/itcast/bservice/web/BServiceController.java
+++ b-service/src/main/java/cn/itcast/bservice/web/BServiceController.java
@@ -1,6 +1,7 @@
 package cn.itcast.bservice.web;
 
 import cn.itcast.bservice.feign.CSserviceFeign;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
