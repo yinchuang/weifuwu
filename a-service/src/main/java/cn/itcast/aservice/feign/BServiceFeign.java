fb378a8565523671d37434aff8afb4ad1076456a
yinchuang <1933740841@qq.com>
2021-06-13 20:35:12 +0800

    add import request header

--- a-service/src/main/java/cn/itcast/aservice/feign/BServiceFeign.java
+++ a-service/src/main/java/cn/itcast/aservice/feign/BServiceFeign.java
@@ -2,6 +2,7 @@ package cn.itcast.aservice.feign;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 
 
