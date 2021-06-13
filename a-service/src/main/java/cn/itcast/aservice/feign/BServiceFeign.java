354c262a9a13785bb8add7f67a176dee7aa1f345
yinchuang <1933740841@qq.com>
2021-06-13 20:51:06 +0800

    add map

--- a-service/src/main/java/cn/itcast/aservice/feign/BServiceFeign.java
+++ a-service/src/main/java/cn/itcast/aservice/feign/BServiceFeign.java
@@ -3,7 +3,7 @@ package cn.itcast.aservice.feign;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
-
+import java.util.Map;
 
 
 @FeignClient(value = "bservice")
