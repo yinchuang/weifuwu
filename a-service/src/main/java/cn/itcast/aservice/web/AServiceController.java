ced991ae03309f399da35b45ec84a03e9ddc119c
yinchuang <1933740841@qq.com>
2021-06-14 07:25:18 +0800

    delete RequestMapping in aservice web

--- a-service/src/main/java/cn/itcast/aservice/web/AServiceController.java
+++ a-service/src/main/java/cn/itcast/aservice/web/AServiceController.java
@@ -3,8 +3,8 @@ package cn.itcast.aservice.web;
 import cn.itcast.aservice.feign.BServiceFeign;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 @RestController
 public class AServiceController {
