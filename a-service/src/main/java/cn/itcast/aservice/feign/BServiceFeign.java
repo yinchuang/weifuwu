34c0073413aee99540cd64d0990f6d99e066ef69
yinchuang <1933740841@qq.com>
2021-06-13 20:59:43 +0800

    update header

--- a-service/src/main/java/cn/itcast/aservice/feign/BServiceFeign.java
+++ a-service/src/main/java/cn/itcast/aservice/feign/BServiceFeign.java
@@ -3,12 +3,11 @@ package cn.itcast.aservice.feign;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
-import java.util.Map;
 
 
 @FeignClient(value = "bservice")
 public interface BServiceFeign {
 
     @GetMapping("/bservice")
-    public String bservice(@RequestHeader Map<String, String> headers);
+    public String bservice(@RequestHeader MultiValueMap<String, String> headers);
 }
--- b-service/src/main/java/cn/itcast/bservice/feign/CServiceFeign.java
+++ b-service/src/main/java/cn/itcast/bservice/feign/CServiceFeign.java
@@ -3,12 +3,11 @@ package cn.itcast.bservice.feign;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
-import java.util.Map;
 
 
 @FeignClient("cservice")
 public interface CServiceFeign {
 
     @GetMapping("/cservice")
-    public String cservice(@RequestHeader Map<String, String> headers);
+    public String cservice(@RequestHeader MultiValueMap<String, String> headers);
 }
